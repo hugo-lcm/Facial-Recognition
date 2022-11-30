95e9769791e859bfc24bb7210eceae2bb4cd79d1
Hugo Leonardo <hugolcm@live.com>
2022-11-30 12:09:45 -0300

    requirements.txt and import of libraries

---
+++ main.py
@@ -0,0 +1,11 @@
+# bibliotecas padroes
+import cv2  # opencv
+import os  # para criar a estrutura de pastas
+import random
+import numpy as np  # para arrays
+from matplotlib import pyplot as plt  # para visualizar as imagens
+
+# tensorflow - functional API
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
+import tensorflow as tf
