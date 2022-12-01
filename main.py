a07366dded88255bdf602cc7d1895bf79de7f3b4
Hugo Leonardo <hugolcm@live.com>
2022-12-01 14:57:05 -0300

    collect negative images

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import tensorflow as tf
 # gpu growth - limitar o tanto de vram que o tensorflow poderá usar
 # só fazer se for rodar o código localmente e com gpu dedicada
 # avoid OOM (out of memory) errors
+
 # gpus = tf.config.experimental.list_physical_devices('GPU') # pega todas as gpu's da maquina
 # for gpu in gpus:
 #     tf.config.experimental.set_memory_growth(gpu, True) # set memory growth
@@ -29,4 +30,16 @@ try:
     os.makedirs(NEG_PATH)
     os.makedirs(ANC_PATH)
 except:
-    print(f'{Fore.RED}pastas já criadas {Style.RESET_ALL}')
+    print(f'{Fore.RED}pastas já criadas{Style.RESET_ALL}')
+
+# collect negative images
+# database de imagens para usar como negative: http://vis-www.cs.umass.edu/lfw/
+# mover todas as imagens para data/negative
+try:
+    for directory in os.listdir('lfw'):
+        for file in os.listdir(os.path.join('lfw', directory)):
+            EX_PATH = os.path.join('lfw', directory, file)
+            NEW_PATH = os.path.join(NEG_PATH, file)
+            os.replace(EX_PATH, NEW_PATH)
+except:
+    print(f'{Fore.RED}arquivos já movidos{Style.RESET_ALL}')
