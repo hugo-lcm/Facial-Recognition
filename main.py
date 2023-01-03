deca61263a91a70cc76fbfada84b64dec7afe111
Hugo Leonardo <hugolcm@live.com>
2023-01-03 14:01:27 -0300

    build train and test partition

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+# 1.2 import dependencies
 # bibliotecas padroes
 import cv2  # opencv
-import os  # para criar a estrutura de pastas
+import os  # criar a estrutura de pastas
 import random
-import numpy as np  # para arrays
-from matplotlib import pyplot as plt  # para visualizar as imagens
+import numpy as np  # arrays
+import matplotlib.pyplot as plt  # visualizar as imagens
 from colorama import Fore, Style
 import uuid  # gerar nomes únicos para as imagens
 
@@ -12,25 +13,26 @@ from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
 import tensorflow as tf
 
-# gpu growth - limitar o tanto de vram que o tensorflow poderá usar
-# só fazer se for rodar o código localmente e com gpu dedicada
+# 1.3 set gpu growth - limitar o tanto de vram que o tensorflow poderá usar
 # avoid OOM (out of memory) errors
+
+
 try:
     gpus = tf.config.experimental.list_physical_devices(
         'GPU')  # pega todas as gpu's da maquina
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(
-            gpu, True)  # set memory growth]
+            gpu, True)  # set memory growth
 except:
     print(f'{Fore.RED}nenhuma gpu dedicada encontrada{Style.RESET_ALL}')
 
-# setup paths
+# 1.4 create folder structures
+
 
 POS_PATH = os.path.join('data', 'positive')  # imagem igual
 NEG_PATH = os.path.join('data', 'negative')  # imagem diferente
 ANC_PATH = os.path.join('data', 'anchor')  # imagem da camera
 
-# criando pastas
 
 try:
     os.makedirs(POS_PATH)
@@ -39,7 +41,9 @@ try:
 except:
     print(f'{Fore.RED}pastas já criadas{Style.RESET_ALL}')
 
-# collect negative images
+# 2 collect positives and anchores
+# 2.1 untar labelled faces in the Wild Dataset
+
 
 # database de imagens para usar como negative: http://vis-www.cs.umass.edu/lfw/
 # mover todas as imagens para data/negative
@@ -52,7 +56,8 @@ try:
 except:
     print(f'{Fore.RED}arquivos já movidos{Style.RESET_ALL}')
 
-# collect positive and anchor images
+# 2.2 collect positive and anchor classes
+
 
 # estabelecendo conexão com a webcam
 # se der erro aqui, tente outros numeros pq pode variar de pc pra pc
@@ -86,23 +91,25 @@ cap.release()
 # fechar janela
 cv2.destroyAllWindows()
 
-# get image directories
+# 3 load and preprocess images
+# 3.1 get image directories
+
 
 anchor = tf.data.Dataset.list_files(ANC_PATH+'/*.jpg').take(300)
 positive = tf.data.Dataset.list_files(POS_PATH+'/*.jpg').take(300)
 negative = tf.data.Dataset.list_files(NEG_PATH+'/*.jpg').take(300)
 
-# preprocessing - scale and resize
+# 3.2 preprocessing - scale and resize
 
 
-def preProcess(file_path):
+def preprocess(file_path):
     byte_img = tf.io.read_file(file_path)
     img = tf.io.decode_jpeg(byte_img)
     img = tf.image.resize(img, (100, 100))
     img /= 255.0
     return img
 
-# create labelled dataset
+# 3.3 create labelled dataset
 
 
 positives = tf.data.Dataset.zip(
@@ -111,7 +118,25 @@ negatives = tf.data.Dataset.zip(
     (anchor, negative, tf.data.Dataset.from_tensor_slices(tf.zeros(len(anchor)))))
 data = positives.concatenate(negatives)
 
-samples = data.as_numpy_iterator()
+# 3.4 build train and test partition
 
-ex = samples.next()
-print(ex)
+
+def preprocess_twin(input_img, validation_img, label):
+    return (preprocess(input_img), preprocess(validation_img), label)
+
+
+# build dataloader pipeline
+data = data.map(preprocess_twin)
+data = data.cache()
+data = data.shuffle(buffer_size=1024)
+
+# training partition
+train_data = data.take(round(len(data)*.7))
+train_data = train_data.batch(16)
+train_data = train_data.prefetch(8)
+
+# testing partition
+test_data = data.skip(round(len(data)*.7))
+test_data = test_data.take(round(len(data)*.3))
+test_data = test_data.batch(16)
+test_data = test_data.prefetch(8)
