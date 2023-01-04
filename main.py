95475502c7b714a8e1777c9caadaad9669544f51
Hugo Leonardo <hugolcm@live.com>
2023-01-04 17:18:41 -0300

    create embedding layer

--- main.py
+++ main.py
@@ -140,3 +140,29 @@ test_data = data.skip(round(len(data)*.7))
 test_data = test_data.take(round(len(data)*.3))
 test_data = test_data.batch(16)
 test_data = test_data.prefetch(8)
+
+# 4 model engineering
+# 4.1 create embedding layer
+
+
+def make_embedding():
+    inp = Input(shape=(100, 100, 3), name='input_image')
+
+    # first block
+    c1 = Conv2D(64, (10, 10), activation='relu')(inp)
+    m1 = MaxPooling2D(64, (2, 2), padding='same')(c1)
+
+    # second block
+    c2 = Conv2D(128, (7, 7), activation='relu')(m1)
+    m2 = MaxPooling2D(64, (2, 2), padding='same')(c2)
+
+    # third block
+    c3 = Conv2D(128, (4, 4), activation='relu')(m2)
+    m3 = MaxPooling2D(64, (2, 2), padding='same')(c3)
+
+    # final embedding layer
+    c4 = Conv2D(256,  (4, 4), activation='relu')(m3)
+    f1 = Flatten()(c4)
+    d1 = Dense(4096, activation='sigmoid')(f1)
+
+    return Model(inputs=[inp], outputs=[d1], name='embedding')
