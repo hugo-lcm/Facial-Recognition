8565da4657ef626f59275a1fd9bdccb60fbb6d03
Hugo Leonardo <hugolcm@live.com>
2023-02-06 18:31:02 -0300

    make siamese model

--- main.py
+++ main.py
@@ -167,6 +167,8 @@ def make_embedding():
 
     return Model(inputs=[inp], outputs=[d1], name='embedding')
 
+embedding = make_embedding()
+
 # 4.2 build distance layer
 
 
@@ -176,3 +178,23 @@ class L1Dist(Layer):
 
     def call(self, input_embedding, validation_embedding):
         return tf.math.abs(input_embedding - validation_embedding)
+
+# 4.3 make siamese model
+
+
+def make_siamese_model():
+    # anchor image input in the network
+    input_image = Input(name='input_img', shape=(100, 100, 3))
+
+    # validation image in the network
+    validation_image = Input(name='validation_img', shape=(100, 100, 3))
+
+    # combine siamese distance components
+    siamese_layer = L1Dist()
+    siamese_layer._name = 'distance'
+    distances = siamese_layer(embedding(input_image), embedding(validation_image))
+
+    # classification layer
+    classifier = Dense(1, activation='sigmoid')(distances)
+
+    return Model(inputs=[input_image, validation_image], outputs=[classifier], name='SiameseNetwork')
